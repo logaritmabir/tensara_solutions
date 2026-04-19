27d3573ba5f573c26af3cc817a5a0608bfb672db
logaritmabir <emre.ozer@bil.omu.edu.tr>
2026-04-19 04:57:31 +0300

    add warp shuffle instrucitons to l1_normalization

--- l1_normalization.cu
+++ l1_normalization.cu
@@ -32,10 +32,53 @@ __global__ void l1_normalization(const float* __restrict__ X, float* __restrict_
     }
 }
 
+__global__ void l1_normalization_warp_shf(const float* __restrict__ X, float* __restrict__ Y, size_t cols){
+    extern __shared__ float s_sum[];
+    
+    size_t row_idx = blockIdx.x;
+    size_t col_idx = threadIdx.x;
+
+    size_t lane_id = col_idx % 32;
+    size_t warp_id = col_idx / 32;
+
+    float local_sum = 0.0f;
+    for(size_t i = col_idx; i < cols; i += blockDim.x){
+        local_sum += fabsf(X[get_linear_index(row_idx, i, cols)]);
+    }
+
+    for(size_t s = 16; s > 0 ; s>>=1){
+        local_sum += __shfl_down_sync(0xFFFFFFFF, local_sum, s);
+    }
+
+    if(lane_id == 0){
+        s_sum[warp_id] = local_sum;
+    }
+    __syncthreads();
+
+    if (warp_id == 0) {
+        float val = (lane_id < (blockDim.x / 32)) ? s_sum[lane_id] : 0.0f;
+
+        for(size_t s = 16; s > 0 ; s>>=1){
+            val += __shfl_down_sync(0xFFFFFFFF, val, s);
+        }
+
+        if (lane_id == 0) s_sum[0] = val;
+    }
+    __syncthreads();
+
+    float normalized = s_sum[0];
+
+    for(size_t i = col_idx; i < cols; i += blockDim.x){
+        size_t pos = get_linear_index(row_idx, i, cols);
+        Y[pos] = X[pos] / normalized;
+    }
+}
+
 // Note: X, Y are device pointers
 extern "C" void solution(const float* X, float* Y, size_t B, size_t D) {
-    size_t threads = 256;
+    size_t threads = 1024;
+    size_t warps_per_block = threads / 32;
     size_t blocks = B;
 
-    l1_normalization<<<blocks,threads, threads * sizeof(float)>>>(X,Y,D);
+    l1_normalization_warp_shf<<<blocks,threads, warps_per_block * sizeof(float)>>>(X,Y,D);
 }
